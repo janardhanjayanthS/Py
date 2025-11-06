26f502bcd730c9c54e2d2ead723b8af944b2d686
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-06 11:24:24 +0530

    W1D6 add read_csv function in main.py

--- Week 1/Day 6/main.py
+++ Week 1/Day 6/main.py
@@ -1,6 +1,20 @@
+from csv import DictReader
 
 
+def read_file(filename: str):
+    """
+    Reads data from csv file
+    Args:
+        filename: csv file to read
+    """
+    try: 
+        with open(filename, 'r') as csv_file:
+            reader = DictReader(csv_file)
+            for row in reader:
+                print(row)
+    except FileNotFoundError as e:
+        print(f'File not found {e}')
 
 
 if __name__ == '__main__':
-    ...
+    read_file('inventory.csv')
