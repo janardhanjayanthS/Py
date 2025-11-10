3b19a484c166af654e121d01516ac9761ddf2cc2
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-10 17:03:35 +0530

    D6 refactored setup.py file to updated version

--- Week 2/Day 6/package/setup.py
+++ Week 2/Day 6/package/setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="inventory_manager",
-    version="0.0.1",
+    version="0.0.2",
     description="Package to interact with inventory data",
+    long_description="Package to interact with inventory data that uses generators to read from inventory.csv file",
     package_dir={"": "inventory_manager"},
     packages=find_packages(where="inventory_manager"),
     author="janardhanjayanthS",
     install_requires=["pydantic>=2.12"],
-    python_requires=">=3.10"
+    python_requires=">=3.10",
 )
-
