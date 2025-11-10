10f140d90f46f38f291bdebef7eb152b4680b4e0
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-10 15:23:11 +0530

    D4 add iterators and generators

---
+++ Week 2/Day 4/iterators_and_generators.py
@@ -0,0 +1,66 @@
+numbers = (1, 5, 6, 7, 8, 3, 55)
+
+num_iterator = iter(numbers)
+# print(num_iterator)
+
+# print(num_iterator.__iter__())
+
+# print(next(num_iterator))
+# print(next(num_iterator))
+# print(next(num_iterator))
+# print(next(num_iterator))
+# print(next(num_iterator))
+# print(next(num_iterator))
+# print(next(num_iterator))
+# print(next(num_iterator))
+
+
+class SquareIterator:
+    def __init__(self, sequence):
+        self._sequence = sequence
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._sequence):
+            square = self._sequence[self._index] ** 2
+            self._index += 1
+            return square
+        else:
+            raise StopIteration
+
+
+sq_seq = SquareIterator([1, 5, 9, 21])
+# print(sq_seq.__iter__())
+
+# print(sq_seq.__next__())
+# print(sq_seq.__next__())
+# print(sq_seq.__next__())
+# print(sq_seq.__next__())
+# print(sq_seq.__next__())
+
+
+# Generators
+
+
+def gen(n):
+    yield n
+    yield n + 1
+
+
+for n in gen(5):
+    print(n)
+
+
+def fibonacci(n):
+    a, b = 0, 1
+    while n:
+        yield b
+        a, b = b, a + b
+        # n -= 1
+
+
+for n in fibonacci(10):
+    print(n)
