07ab0c5cad3bb759eb36a078789fdd1077aed216
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-13 16:11:23 +0530

    add inventory_manager package data to Week 3 dir, update .gitignore to exclude some files

---
+++ Week 3/Day 6 EOW project/package/inventory_manager/inventory_manager/src/file_manager.py
@@ -0,0 +1,73 @@
+from .config import ConfigLoader
+from .model import BaseProduct
+
+
+config: ConfigLoader = ConfigLoader()
+
+
+def check_low_stock_or_print_details(product: BaseProduct) -> None:
+    """
+    Checks if prodcut is low, if so then appends it to low stock report
+    else prints product detail
+    Args:
+        product: object of Product form model.py
+    """
+    if product:
+        if check_low_stock(product_quantity=product.quantity):
+            append_low_stock_report(product=product)
+        else:
+            print(product)
+
+
+def check_low_stock(product_quantity: int) -> bool | None:
+    """
+    Checks if the quantity of a product is than 10
+    Args:
+        product_quantity: quantity of product
+    Returns:
+        True if quantity is less than 10, False otherwise
+    """
+    try:
+        if int(product_quantity) < config.get_low_quality_threshold():
+            return True
+        return False
+    except TypeError as e:
+        print(f"Encountered type error: {e}")
+
+
+def append_low_stock_report(product: BaseProduct) -> None:
+    """
+    Writes low stock data to low_stock_report.txt
+    if it exists otherwise creates then writes
+    Args:
+        product: object of Product form model.py
+    """
+    print(f"Product '{product.product_name}' is available in less quantity {product.quantity}")
+    filename = "low_stock_report.txt"
+    try:
+        append_content(filename, f'{product}')
+    except FileNotFoundError:
+        print("low_stock_report.txt does not exist")
+        create_file(filename=filename)
+        append_content(filename, f'{product}')
+
+
+def append_content(filename: str, content: str) -> None:
+    """
+    Writes content to file
+    Args:
+        filename: file to write
+        content: content to write
+    """
+    with open(filename, "a") as file:
+        file.write(content + "\n")
+
+
+def create_file(filename: str) -> None:
+    """
+    creates a file with provided file name
+    Args:
+        filename: name of file to create
+    """
+    with open(filename, "x") as _:
+        print(f"File {filename} created successfully")
---
+++ Week 3/Day 6 EOW project/package/inventory_manager/inventory_manager/src/model.py
@@ -0,0 +1,123 @@
+from pydantic import BaseModel, PositiveFloat, PositiveInt, Field
+from datetime import datetime, timedelta
+from abc import ABC
+
+from .utility import ProductDetails, ProductTypes
+
+
+class BaseProduct(ABC, BaseModel):
+    """
+    Defines a product model
+    Attributes:
+        product_id: id of the product
+        product_name: name of the product
+        quantity: quantity of the product
+        price: price of the product
+    """
+
+    product_id: str
+    product_name: str = Field(..., min_length=1)
+    quantity: PositiveInt
+    price: PositiveFloat
+    type: ProductTypes
+
+    def __str__(self):
+        message = ""
+        for key, value in self.__dict__.items():
+            if key == "type":
+                message += f"{key}: {value.value} | "
+            else:
+                message += f"{key}: {value} | "
+        return message
+
+
+class RegularProduct(BaseProduct):
+    """
+    Initializes regular product
+    Args:
+        product_details: dataclass conatining product information
+    """
+
+
+class FoodProduct(BaseProduct):
+    """
+    Initializes a food product
+    Args:
+        product_details: dataclass conatining product information
+    Additional Attributes:
+        days_to_expire: food's expiry period in days
+        is_vegetarian: food's classification
+    """
+
+    days_to_expire: PositiveInt
+    is_vegetarian: bool
+
+    def get_expiry_date(self) -> str:
+        """
+        returns exact expiry date
+        Returns
+            int representing expiry days
+        """
+        return datetime.strftime(
+            timedelta(days=self.days_to_expire) + datetime.now(),  # type: ignore
+            "%d-%m-%Y",  # type: ignore
+        )
+
+
+class ElectronicProduct(BaseProduct):
+    """
+    Initializes electronic product
+    Args:
+        product_details: dataclass conatining product information
+    Additional Attributes:
+        warrenty_period_in_years: in years
+    """
+
+    warranty_period_in_years: float
+
+
+class ProductFactory:
+    """
+    Manages product types
+    """
+
+    def create_product(self, product_details: ProductDetails) -> BaseProduct:
+        """
+        Creates and returns product object based on product type
+
+        Args:
+            product_details : contains details about the product
+
+        Returns:
+            BaseProduct: the created object type
+        """
+        if product_details.type == ProductTypes.RP.value:
+            return RegularProduct(
+                product_id=product_details.id,
+                product_name=product_details.name,
+                quantity=product_details.quantity,
+                price=product_details.price,
+                type=product_details.type,
+            )
+        elif product_details.type == ProductTypes.FP.value:
+            return FoodProduct(
+                product_id=product_details.id,
+                product_name=product_details.name,
+                quantity=product_details.quantity,
+                price=product_details.price,
+                days_to_expire=product_details.days_to_expire,
+                is_vegetarian=product_details.is_vegetarian,  # type: ignore
+                type=product_details.type,
+            )
+        elif product_details.type == ProductTypes.EP.value:
+            return ElectronicProduct(
+                product_id=product_details.id,
+                product_name=product_details.name,
+                quantity=product_details.quantity,
+                price=product_details.price,
+                warranty_period_in_years=product_details.warranty_period_in_years,
+                type=product_details.type,
+            )
+        else:
+            print(f"Requested product type: {type} not available")
+            return  # type: ignore
