4aabc91464ed77c3e00c35a7e377300baf9dc4d4
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-17 16:14:03 +0530

    refactor: moved tool functions into a separate file to mitigate circular imports problem

--- courses/prompt engi. + pydantic/src/main.py
+++ courses/prompt engi. + pydantic/src/main.py
@@ -1,13 +1,32 @@
 from openai import OpenAI
 from typing import Any
 from prompts import system_prompt
+from utility import validate_user_input
+from tools import tool_definition
 
 client = OpenAI()
 
 tasks: list = []
 
 
-messages: list[dict[str, Any]] = [
-    {"role": "system", "content": system_prompt},
-    {"role": "user", "content": ""},
-]
+user_input_json = """
+{
+    "id": 1,
+    "task": "buy grocery",
+    "done": False
+}
+"""
+
+valid_user_input = validate_user_input(user_json=user_input_json)
+
+# messages: list = [
+#     {"role": "system", "content": system_prompt},
+#     {"role": "user", "content": valid_user_input},
+# ]
+
+# response = client.chat.completions.create(
+#     model="chatgpt-4o-latest",
+#     messages=messages,
+#     tools=tool_definition,
+#     tool_choice='auto'
+# )
--- courses/prompt engi. + pydantic/src/tools.py
+++ courses/prompt engi. + pydantic/src/tools.py
@@ -1,6 +1,5 @@
 from typing import Any
 from model import AddTaskInput, ListTasksInput, MarkDoneInput
-from main import tasks
 
 tool_definition: list[Any] = [
     {
@@ -29,45 +28,3 @@ tool_definition: list[Any] = [
     },
 ]
 
-
-def add_task(add_args: AddTaskInput):
-    """
-    adds a new task to tasks list
-
-    Args:
-        add_args: contains infromation about task
-    """
-    tasks.append(add_args)
-
-
-def list_tasks():
-    """
-    Lists all existing tasks from tasks list
-    """
-    if tasks:
-        print_tasks()
-    else:
-        print("Tasks list is empty")
-
-
-def print_tasks() -> None:
-    """
-    Prints each task line by line
-    """
-    for task in tasks:
-        print(f"id: {task.id} | work: {task.task} | is done: {task.done}")
-
-
-def mark_done(mark_done_args: MarkDoneInput):
-    """
-    Marks a task done/compeleted if it exists or
-    prints unable to find task.
-
-    Args:
-        mark_done_args: contains the id of the ask to update
-    """
-    for task in tasks:
-        if task.id == mark_done_args.task_id:
-            task.done = True
-            return
-    print(f"Unable to find the requested task with id: {mark_done_args.task_id}")
