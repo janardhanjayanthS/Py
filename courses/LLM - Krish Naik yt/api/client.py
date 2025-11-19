52dbc291054625319997129290dbf228b58a8198
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-19 14:39:11 +0530

    bugfix: for returning output content from ollama response

--- courses/LLM - Krish Naik yt/api/client.py
+++ courses/LLM - Krish Naik yt/api/client.py
@@ -4,28 +4,24 @@ import requests
 
 def get_opneai_response(topic: str) -> str:
     response = requests.post(
-        "http://localhost:5001/essay/invoke", json={input: {"topic": topic}}
+        "http://localhost:5001/essay/invoke", json={"input": {"topic": topic}}
     )
-    print(response)
     return response.json()["output"]["content"]
 
 
 def get_ollama_response(topic: str) -> str:
     response = requests.post(
-        "http://localhost:5001/poem/invoke", json={input: {"topic": topic}}
+        "http://localhost:5001/poem/invoke", json={"input": {"topic": topic}}
     )
-    print(response)
-    return response.json()["output"]["content"]
+    return response.json()["output"]
 
 
-get_opneai_response("car")
-get_ollama_response("car")
 
-# st.title("LLM poet/essayist")
-# essay_topic = st.text_input("Enter topic to write an essay")
-# poem_topic = st.text_input("Enter topic to write a poem")
+st.title("LLM poet/essayist")
+essay_topic = st.text_input("Enter topic to write an essay")
+poem_topic = st.text_input("Enter topic to write a poem")
 
-# if essay_topic:
-#     st.write(get_opneai_response(topic=essay_topic))
-# if poem_topic:
-#     st.write(get_ollama_response(topic=poem_topic))
+if essay_topic:
+    st.write(get_opneai_response(topic=essay_topic))
+if poem_topic:
+    st.write(get_ollama_response(topic=poem_topic))
