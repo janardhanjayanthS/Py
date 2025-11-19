258c196bbd62c71d6fcff776ddfe1ebe36fd12be
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-19 13:15:48 +0530

    add: new local_app that uses ollama gemma3 model, add edpoints to app.py in api dir

---
+++ courses/LLM - Krish Naik yt/api/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
+from langserve import add_routes
+import uvicorn
+import os
+
+app = FastAPI(
+    title="Langchain server",
+    version='0.0.1',
+    description='backend for a llm'
+)
+
+
+add_routes(
+    app, ChatOpenAI(), path='/openai'
+)
+
+openai_model = ChatOpenAI()
+ollama_model = Ollama(model='gemma3')
+
+prompt_for_openai_model = ChatPromptTemplate.from_template('Create an essay with {} as topic with 100 words')
+prompt_for_ollama_model = ChatPromptTemplate.from_template('Create an poem with {} as topic with 100 words')
+
+add_routes(app, prompt_for_openai_model | openai_model, path='/essay')
+
+add_routes(app, prompt_for_ollama_model | ollama_model, path='/poem')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=5001)
---
+++ courses/LLM - Krish Naik yt/app/local_app.py
@@ -0,0 +1,37 @@
+"""
+Similar to app.py but implemented with ollama locally
+"""
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama  
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = str(os.getenv("LANGCHAIN_API_KEY"))
+
+# Prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "you are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question: {question}"),
+    ]
+)
+
+
+# Streamlit framework
+st.title("Langchain with Ollama which runs locally - gemma3")
+input_text = st.text_input("User! give some Query...")
+
+# OpenAI LLM
+llm = Ollama(model='gemma3')
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({'question': input_text}))
+
