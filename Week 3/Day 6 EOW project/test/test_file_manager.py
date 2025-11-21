6997a153a3e1c5356db6294620a08cac59a6d6cd
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-21 18:37:31 +0530

    test: add for append_content with mock file operations

--- Week 3/Day 6 EOW project/test/test_file_manager.py
+++ Week 3/Day 6 EOW project/test/test_file_manager.py
@@ -1,4 +1,5 @@
-from inventory_manager import check_low_stock
+from inventory_manager import check_low_stock, append_content
+from unittest.mock import patch, mock_open
 import pytest
 
 
@@ -22,8 +23,7 @@ class TestCheckLowStock:
         high_qty_result = check_low_stock(high_qty)
 
         assert high_qty_result is False
-    
-        
+
     def test_check_low_stock_as_low_qty_threshold(self):
         """
         test when qty is same as thershold(10)
@@ -32,14 +32,26 @@ class TestCheckLowStock:
 
         result = check_low_stock(qty)
 
-        assert result is False 
-    
+        assert result is False
 
     def test_check_low_stock_with_unknown_type(self):
         """
         test for check_low_stock with non int args
         """
         with pytest.raises(ValueError):
-            check_low_stock('abc') # type: ignore
+            check_low_stock("abc")  # type: ignore
 
-    
+
+class TestAppendContent:
+    def test_apppend_content_success(self):
+        """
+        testing appen_content() successful call
+        """
+        m = mock_open()
+        with patch("builtins.open", m):
+            append_content("test.txt", "test content")
+
+        m.assert_called_once_with("test.txt", "a")
+
+        handle = m()
+        handle.write.assert_called_once_with("test content\n")
