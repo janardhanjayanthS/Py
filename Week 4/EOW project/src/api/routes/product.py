ec7abeae10b32b3cd82cbaafcacc17dd4d6cbf21
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-27 18:03:58 +0530

    api(product): POST new product to db with pydantic validation

--- Week 4/EOW project/src/api/routes/product.py
+++ Week 4/EOW project/src/api/routes/product.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from src.core.db_config import get_db
 from src.models.models import Product
+from src.schema.product import ProductCreate, ProductResponse
 
 product = APIRouter()
 
@@ -21,3 +22,25 @@ async def product_with_id(product_id: str, db: Session = Depends(get_db)):
         return {"response": f"product with id {product_id} not found"}
 
     return product
+
+
+@product.post("/add_product", response_model=ProductResponse)
+async def add_product(product: ProductCreate, db: Session = Depends(get_db)):
+    print(product)
+
+    existing_product = db.query(Product).filter_by(id=product.id).first()
+
+    if existing_product is not None:
+        raise HTTPException(status_code=400, detail="Product id already exists")
+
+    if product and product.type == "regular":
+        product.is_vegetarian = product.days_to_expire = product.warranty_in_years = (
+            None
+        )
+
+    db_product = Product(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+
+    return db_product
---
+++ Week 4/EOW project/src/schema/product.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field, PositiveFloat, PositiveInt, field_validator
+
+PRODUCT_TYPES = {"food", "regular", "electronic"}
+
+
+class BaseProduct(BaseModel):
+    """
+    Schema to create attributes
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    id: str
+    name: str = Field(min_length=5, max_length=100)
+    quantity: PositiveInt
+    price: PositiveFloat
+    type: str
+
+
+class ProductCreate(BaseProduct):
+    """
+    Schema for creating POST new product to
+    the db
+
+    Args:
+        BaseProduct: base class with product attributes
+
+    Raises:
+        ValueError: if validation checks fail
+    """
+
+    days_to_expire: Optional[int] = None
+    is_vegetarian: Optional[bool] = None
+    warranty_in_years: Optional[float] = None
+
+    @field_validator("type")
+    @classmethod
+    def validate_product_type(cls, t: str) -> str:
+        """
+        Custom validation to check if given product
+        is one of existing product type
+
+        Args:
+            t: product type
+
+        Raises:
+            ValueError: if given unknown type
+
+        Returns:
+            str: product type
+        """
+        if t not in PRODUCT_TYPES:
+            raise ValueError(f"Product must be of the following types: {PRODUCT_TYPES}")
+        return t
+
+
+class ProductResponse(BaseProduct):
+    class Config:
+        orm_mode = True
+        from_attributes = True
