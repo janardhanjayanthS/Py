45cdbb277e18d0fa594f1298b836e946e5fa2796
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-28 12:04:35 +0530

    update(product api): condensed routes to a single function

--- Week 4/EOW project/src/api/routes/product.py
+++ Week 4/EOW project/src/api/routes/product.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 
-from src.core.api_utility import add_commit_refresh_db, check_if_product_exists
+from src.core.api_utility import get_all_products, get_specific_product, post_product
 from src.core.db_config import get_db
 from src.core.log import log_error
 from src.models.models import Product
@@ -11,36 +13,54 @@ product = APIRouter()
 
 
 @product.get("/products")
-async def products(db: Session = Depends(get_db)):
-    products = db.query(Product).all()
-    return {"status": "success", "message": {"products": products}}
+@product.post("/products")
+async def products(
+    request: Request,
+    product_id: Optional[str] = "",
+    product: Optional[ProductCreate] = None,
+    db: Session = Depends(get_db),
+):
+    if request.method == "POST":
+        return post_product(product=product, db=db)
+    elif request.method == "GET":
+        if product_id and product_id is not None:
+            return get_specific_product(product_id=product_id, db=db)
+        return get_all_products(db=db)
+    return {
+        "status": "error",
+        "message": {"response": f"Unrecognized HTTP method {request.method}"},
+    }
 
 
-@product.get("/products/{product_id}")
-async def product_with_id(product_id: str, db: Session = Depends(get_db)):
-    product = db.query(Product).filter_by(id=product_id).first()
-
-    if product is None:
-        message = f"product with id {product_id} not found"
-        log_error(message=message)
-        return {
-            "status": "error",
-            "message": {
-                "response": message,
-            },
-        }
-
-    return {"status": "success", "message": {"product": product}}
+# @product.get("/products")
+# async def products(db: Session = Depends(get_db)):
 
 
-@product.post("/add_product", response_model=ProductResponse)
-async def add_product(product: ProductCreate, db: Session = Depends(get_db)):
-    check_if_product_exists(product=product, db=db)
+# @product.get("/products/{product_id}")
+# async def product_with_id(product_id: str, db: Session = Depends(get_db)):
+#     product = db.query(Product).filter_by(id=product_id).first()
 
-    if product and product.type == "regular":
-        product.reset_regular_product_attributes()
+#     if product is None:
+#         message = f"product with id {product_id} not found"
+#         log_error(message=message)
+#         return {
+#             "status": "error",
+#             "message": {
+#                 "response": message,
+#             },
+#         }
 
-    db_product = Product(**product.model_dump())
-    add_commit_refresh_db(object=db_product, db=db)
+#     return {"status": "success", "message": {"product": product}}
 
-    return {"status": "success", "message": {"inserted_product": db_product}}
+
+# @product.post("/add_product", response_model=ProductResponse)
+# async def add_product(product: ProductCreate, db: Session = Depends(get_db)):
+#     check_if_product_exists(product=product, db=db)
+
+#     if product and product.type == "regular":
+#         product.reset_regular_product_attributes()
+
+#     db_product = Product(**product.model_dump())
+#     add_commit_refresh_db(object=db_product, db=db)
+
+#     return {"status": "success", "message": {"inserted_product": db_product}}
--- Week 4/EOW project/src/core/api_utility.py
+++ Week 4/EOW project/src/core/api_utility.py
@@ -1,13 +1,13 @@
 from fastapi import HTTPException
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
+from src.core.db_utility import add_commit_refresh_db
 from src.core.log import log_error
 from src.models.models import Product
 from src.schema.product import ProductCreate
 
 
-def check_if_product_exists(product: ProductCreate, db: Session):
+def check_if_product_exists(product: ProductCreate | None, db: Session):
     """
     Checks if product already exists in db,
     if so raises HTTP error
@@ -23,14 +23,77 @@ def check_if_product_exists(product: ProductCreate, db: Session):
         raise HTTPException(status_code=400, detail=message)
 
 
-def add_commit_refresh_db(object: BaseModel, db: Session):
+def post_product(product: ProductCreate | None, db: Session) -> dict:
     """
-    add, commit and refresh db after adding a row
+    Inserts product into db
 
     Args:
-        object: to insert to db (pydantic model instance)
-        db: database instance in session
+        product: Pydnatic product model
+        db: sqlalchemy db object
+
+    Returns:
+        dict: fastapi response
     """
-    db.add(object)
-    db.commit()
-    db.refresh(object)
+    check_if_product_exists(product=product, db=db)
+
+    if product and product.type == "regular":
+        product.reset_regular_product_attributes()
+
+    db_product = Product(**product.model_dump())
+    add_commit_refresh_db(object=db_product, db=db)
+
+    return {"status": "success", "message": {"inserted_product": db_product}}
+
+
+def get_all_products(db: Session) -> dict:
+    """
+    To fetch all products from database
+
+    Args:
+        db: sqlalchemy db object
+
+    Returns:
+        dict: fastapi response
+    """
+    products = db.query(Product).all()
+    return {"status": "success", "message": {"products": products}}
+
+
+def get_specific_product(product_id: str, db: Session) -> dict:
+    """
+    Fetches a specific product from db
+
+    Args:
+        product_id: id to the product to select
+        db: sqlalchemy db object
+
+    Returns:
+        dict: fastapi response
+    """
+    product = db.query(Product).filter_by(id=product_id).first()
+    ensure_product_found(product_id=str(product.id), product=product) # type: ignore
+
+    return {"status": "success", "message": {"product": product}}
+
+
+def ensure_product_found(product_id: str, product: Product):
+    """
+    ensures if db returned a valid product,
+    if not then logs & retruns error
+
+    Args:
+        product_id: fetched product id
+        product: fetched product sqlalchemy object
+
+    Returns:
+        dict: fastapi response
+    """
+    if product is None:
+        message = f"product with id {product_id} not found"
+        log_error(message=message)
+        return {
+            "status": "error",
+            "message": {
+                "response": message,
+            },
+        }
