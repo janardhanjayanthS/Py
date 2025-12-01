51170fae32e08f4e8a03e87956001c71557e75d1
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-01 15:27:46 +0530

    feat(schema): add Token pydantic models

--- Week 4 & 5/EOW project/src/core/jwt_config.py
+++ Week 4 & 5/EOW project/src/core/jwt_config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-JWT_SECRET_KEY = getenv('JWT_SECRET_KEY')
+JWT_SECRET_KEY = getenv('JWT_SECRET_KEY', '')
 ALGORITHM = "SHA256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- Week 4 & 5/EOW project/src/core/jwt_utility.py
+++ Week 4 & 5/EOW project/src/core/jwt_utility.py
@@ -1,1 +1,29 @@
-from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from typing import Optional
+
+from jose import JWTError, jwt, TokenData
+
+from src.core.jwt_config import ALGORITHM, JWT_SECRET_KEY
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    """
+    Creates a JWT token with user data
+
+    Args:
+        data: user's data (eg: name, email)
+        expires_delta: token expiry time. Defaults to None.
+    """
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=30)
+
+    to_encode["exp"] = expire
+
+    encode_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encode_jwt
+
+def decode_access_token(token: str) -> 
