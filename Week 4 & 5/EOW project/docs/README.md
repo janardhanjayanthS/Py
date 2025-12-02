eb75b1828f039c7ff4a300335ca1dbc521667885
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-02 17:36:39 +0530

    docs(alembic): add blog, cmds in docs. update(models): to use mapped cols

--- Week 4 & 5/EOW project/docs/README.md
+++ Week 4 & 5/EOW project/docs/README.md
@@ -1,3 +1,8 @@
+\*\*\* Alembic:
+commands:
+
+1. alembic check: to check changes in models
+
 \*\*\* Blog:
 
 - alembic: https://www.nashruddinamin.com/blog/how-to-use-alembic-for-database-migrations-in-your-fastapi-application
--- Week 4 & 5/EOW project/src/models/models.py
+++ Week 4 & 5/EOW project/src/models/models.py
@@ -1,25 +1,28 @@
-from sqlalchemy import Column, String, Integer, Float, Boolean
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.database import Base
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), unique=True, nullable=False)
-    email = Column(String(150), unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
 
 
 class Product(Base):
-    __tablename__ = 'product'
+    __tablename__ = "product"
 
-    id = Column(String(10), primary_key=True, unique=True, index=True)
-    name = Column(String(100), nullable=False)
-    quantity = Column(Integer, nullable=False)
-    price = Column(Float, nullable=False)
-    type = Column(String(10), nullable=False)
-    days_to_expire = Column(Integer, nullable=True)
-    is_vegetarian = Column(Boolean, nullable=True)
-    warranty_in_years = Column(Float, nullable=True)
+    id: Mapped[str] = mapped_column(
+        String(10), primary_key=True, unique=True, index=True
+    )
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    type: Mapped[str] = mapped_column(String(10), nullable=False)
+    days_to_expire: Mapped[int | None] = mapped_column(nullable=True)
+    is_vegetarian: Mapped[bool | None] = mapped_column(nullable=True)
+    warranty_in_years: Mapped[float | None] = mapped_column(nullable=True)
