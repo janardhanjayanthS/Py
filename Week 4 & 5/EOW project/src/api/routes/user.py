e263e99466801ac07eb468a5068b4aa6005adcf3
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-02 12:42:10 +0530

    feat(user api): add get_all_users route

--- Week 4 & 5/EOW project/src/api/routes/user.py
+++ Week 4 & 5/EOW project/src/api/routes/user.py
@@ -4,7 +4,9 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from src.core.api_utility import authenticate_user, check_existing_user_using_email
+from src.core.constants import ResponseStatus
 from src.core.database import add_commit_refresh_db, get_db, hash_password
+from src.core.decorators import get_current_user
 from src.core.jwt import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from src.models.models import User
 from src.schema.user import UserLogin, UserRegister
@@ -49,3 +51,14 @@ async def login_user(user: UserLogin, db: Session = Depends(get_db)):
     # cannot copy from postman
     print(access_token)
     return {"access_tokem": access_token, "token_type": "Bearer"}
+
+
+@user.get("/user/all")
+async def get_all_users(
+    current_user: User = Depends(get_current_user), db: Session = Depends(get_db)
+):
+    all_users = db.query(User).all()
+    return {
+        "response": ResponseStatus.S.value,
+        "message": {"user email": current_user.email, "users": all_users},
+    }
