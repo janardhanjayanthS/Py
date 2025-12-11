5f1d9165fb867ab0f48ce58ae03bc0db51fb1f7d
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-11 11:59:58 +0530

    feat(db, main): setup pgvector

--- Week 6/Day 6/main.py
+++ Week 6/Day 6/main.py
@@ -1,2 +1,18 @@
+from constants import OPENAI_API_KEY, connection
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
 
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=OPENAI_API_KEY)
+collection_name = "my_docs"
+
+vector = PGVector(
+    embeddings=embeddings,
+    collection_name=collection_name,
+    connection=connection,
+    use_jsonb=True,
+)
+
+
+if __name__ == "__main__":
+    print(embeddings)
 
