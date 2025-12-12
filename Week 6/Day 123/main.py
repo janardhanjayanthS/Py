645b9d1d6714aba40d89c2aa9afd3982e8d786aa
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-12 16:04:22 +0530

    update(token cost calc): for new api

--- Week 6/Day 123/main.py
+++ Week 6/Day 123/main.py
@@ -1,9 +1,10 @@
 from decimal import Decimal
 from typing import Optional
 
-from constants import GPT_4_MODELS, GPT_5_MODELS, client
+from constants import GPT_4_MODELS, GPT_5_MODELS, OPENAI_API_KEY
 from evaluation import evaluate_llm_response
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
 from openai.types.responses import Response
 from prompt import SYSTEM_PROMPT_FEW_SHOT
 from utility import calculate_token_cost
@@ -15,20 +16,32 @@ def get_completion_from_messages(
     print(f"Model called using: {model}")
     if model in GPT_4_MODELS:
         try:
-            response = client.chat.completions.create(
+            gpt_4_client = ChatOpenAI(
                 model=model,
-                messages=messages,
                 temperature=temperature,
                 max_tokens=max_tokens,
+                api_key=OPENAI_API_KEY,
+            )
+            ai_response = gpt_4_client.invoke(
+                input=messages,
+            )
+            return get_response_content_and_print_token_cost(
+                response=ai_response, model=model, messages=messages
             )
         except Exception as e:
             print(f"Error: {e}")
     elif model in GPT_5_MODELS:
         try:
-            response = client.chat.completions.create(
+            gpt_5_client = ChatOpenAI(
                 model=model,
-                messages=messages,
                 reasoning_effort="medium",
+                api_key=OPENAI_API_KEY,
+            )
+            ai_response = gpt_5_client.invoke(
+                input=messages,
+            )
+            return get_response_content_and_print_token_cost(
+                response=ai_response, model=model, messages=messages
             )
         except Exception as e:
             print(f"Error: {e}")
@@ -36,9 +49,13 @@ def get_completion_from_messages(
         print(f"Model {model} not found")
         return
 
-    llm_response = response.choices[0].message.content
+
+def get_response_content_and_print_token_cost(
+    response: Response, model: str, messages: list
+) -> str:
+    llm_response = response.content
     evaluation_score = evaluate_llm_response(
-        llm_response=llm_response, user_query=messages[-1]["content"]
+        llm_response=llm_response, user_query=messages[-1].content
     )
     print(f"Evaluation score: {evaluation_score}")
     print("cost: $", Decimal(calculate_token_cost(response=response, model_name=model)))
