d2080601abb6c1a8796cf756a5c169d498aa1381
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-12 16:38:10 +0530

    update(main, utility): add logger + exception handling

--- Week 6/Day 123/main.py
+++ Week 6/Day 123/main.py
@@ -1,7 +1,8 @@
+import logging
 from decimal import Decimal
 from typing import Optional
 
-from constants import GPT_4_MODELS, GPT_5_MODELS, OPENAI_API_KEY
+from constants import GPT_4_MODELS, GPT_5_MODELS, OPENAI_API_KEY, logger
 from evaluation import evaluate_llm_response
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
@@ -9,11 +10,13 @@ from openai.types.responses import Response
 from prompt import SYSTEM_PROMPT_FEW_SHOT
 from utility import calculate_token_cost
 
+logging.basicConfig(level=logging.INFO)
+
 
 def get_completion_from_messages(
     messages, model="gpt-3.5-turbo", temperature=0, max_tokens=500
 ) -> Optional[Response]:
-    print(f"Model called using: {model}")
+    logger.info(f"Model called using: {model}")
     if model in GPT_4_MODELS:
         try:
             gpt_4_client = ChatOpenAI(
