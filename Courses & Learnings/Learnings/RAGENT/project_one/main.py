a36fd3534c8b509a8b25d107604d7236d92b2644
janardhanjayanthS <janardhanjayanth@bitcot.com>
2026-01-08 12:13:52 +0530

    feat(main): add mainloop

--- Courses & Learnings/Learnings/RAGENT/project_one/main.py
+++ Courses & Learnings/Learnings/RAGENT/project_one/main.py
@@ -1,9 +1,26 @@
 from constants import llm
 from langchain.agents import create_agent
+from langchain.messages import HumanMessage
 from prompts import MAIN_AGENT_SYSTEM_PROMPT
 from tools import number_calculation, process_medical_query
 
-agent = create_agent(llm, system_prompt=MAIN_AGENT_SYSTEM_PROMPT)
-response = agent.invoke(tools=[number_calculation, process_medical_query])
+agent = create_agent(
+    llm,
+    tools=[number_calculation, process_medical_query],
+    system_prompt=MAIN_AGENT_SYSTEM_PROMPT,
+)
 
-print(response)
+
+if __name__ == "__main__":
+    config = {"configurable": {"thread_id": "one"}}
+
+    print("AI that answers basic arithmentic questions and medical qeueries")
+    while True:
+        user_input = input("user query: (or) 'e' to exit")
+        if user_input.lower() in ["e", "exit"]:
+            break
+        messages = [HumanMessage(content=user_input)]
+        response = agent.invoke({"messages": messages}, config=config)
+        print(f"AI: {response.content}")
+
+    print("EXIT")
