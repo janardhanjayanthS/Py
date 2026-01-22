d899902c0af3d40434f0a37fb8e15b348b299ae9
janardhanjayanthS <janardhanjayanth@bitcot.com>
2026-01-22 17:58:59 +0530

    feat(vector manager): implemented vector manager for sbert + openai

--- Week N/SBERT/project_one/src/docs.py
+++ Week N/SBERT/project_one/src/docs.py
@@ -1,15 +1,4 @@
 import os
-from typing import Any
 
-import pymupdf  # noqa: F401
-from langchain_community.document_loaders import PyMuPFDLoader
-
-corpus: list[str | list[Any] | dict[Any, Any]] = []
 pdf_dir: str = os.getcwd() + "/../data/new_pdfs"
 pdf_files = os.listdir(pdf_dir)
-
-for pdf in pdf_files:
-    doc = pymupdf.open(pdf_dir + f"/{pdf}")
-
-    new_doc = PyMuPFDLoader(pdf_dir + f"/{pdf}")
-    print(new_doc)
--- Week N/SBERT/project_one/src/main.py
+++ Week N/SBERT/project_one/src/main.py
@@ -8,6 +8,8 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_postgres.vectorstores import PGVector
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from .docs import pdf_dir, pdf_files
+
 _ = load_dotenv()
 
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
@@ -59,5 +61,24 @@ class VectorManager:
         return results
 
 
+def add_pdfs_for_openai(manager: VectorManager) -> None:
+    for pdf_file in pdf_files:
+        manager.store_pdf(file_path=f"{pdf_dir}/{pdf_file}", model_type="openai")
+
+
+def add_pdfs_for_sbert(manager: VectorManager) -> None:
+    for pdf_file in pdf_files:
+        manager.store_pdf(file_path=f"{pdf_dir}/{pdf_file}")
+
+
 if __name__ == "__main__":
-    ...
+    v_manager = VectorManager()
+    add_pdfs_for_openai(manager=v_manager)
+    add_pdfs_for_sbert(manager=v_manager)
+
+    llm_query = "what is medical insurance"
+    print("Results from Openai")
+    print(v_manager.query(query=llm_query, model_type="openai"))
+    print("-" * 100)
+    print("Results from SBERT")
+    print(v_manager.query(query=llm_query, model_type="sbert"))
