597425d8059c6bafc0961115a44827aa5b7f1e66
janardhanjayanthS <janardhanjayanth@bitcot.com>
2026-01-22 11:58:09 +0530

    chore(req.txt): add req.txt

--- Week N/Docker/project_one/src/main.py
+++ Week N/Docker/project_one/src/main.py
@@ -1,13 +1,19 @@
+from os import getenv
+
 import uvicorn
+from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
+
+load_dotenv(find_dotenv())
 
 app = FastAPI()
 
 
 @app.get("/")
 async def hw() -> dict[str, str]:
+    print(f"ENV VAR: {getenv('sample')}")
     return {"response": "jello world"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5003)
+    uvicorn.run(app, host=getenv("host", "0.0.0.0"), port=int(getenv("port", 5003)))
