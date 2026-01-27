f77d3e934083742a8cd6f3a57817b5804a495788
janardhanjayanthS <janardhanjayanth@bitcot.com>
2026-01-27 10:42:38 +0530

    chore(interfaces): extend user repo, service interface

--- Week 4 & 5/SDLC/src/interfaces/user_repo.py
+++ Week 4 & 5/SDLC/src/interfaces/user_repo.py
@@ -1,9 +1,18 @@
 from abc import ABC, abstractmethod
 
+from src.models.user import User
 from src.schema.user import UserRegister, UserResponse
 
 
 class AbstractUserRepository(ABC):
     @abstractmethod
     def create_user(self, user: UserRegister) -> UserResponse:
+        pass
+
+    @abstractmethod
+    def delete_user(self, user_id: int) -> User | dict:
+        pass
+
+    @abstractmethod
+    def fetch_all_users(self) -> list:
         pass
