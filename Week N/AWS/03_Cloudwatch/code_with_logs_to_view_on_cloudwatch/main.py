1c7a6f9f6eb077bcbd343e50a16f8c06e8846097
janardhanjayanthS <janardhanjayanth@bitcot.com>
2026-02-26 13:25:03 +0530

    update(main.py): add logs

--- Week N/AWS/03_Cloudwatch/code_with_logs_to_view_on_cloudwatch/main.py
+++ Week N/AWS/03_Cloudwatch/code_with_logs_to_view_on_cloudwatch/main.py
@@ -33,24 +33,29 @@ handler = Mangum(app)
 
 @app.get("/")
 async def root():
+    logger.info("Hit root endpoint!!!")
     return {"message": "Welcome to my bookstore app!"}
 
 
 @app.get("/random-book")
 async def random_book():
+    logger.info("returning a random books")
     return random.choice(BOOKS)
 
 
 @app.get("/list-books")
 async def list_books():
+    logger.info("listing all books")
     return {"books": BOOKS}
 
 
 @app.get("/book_by_index/{index}")
 async def book_by_index(index: int):
     if index < len(BOOKS):
+        logger.info("Book index found")
         return BOOKS[index]
     else:
+        logger.error("Book index out of range")
         raise HTTPException(404, f"Book index {index} out of range ({len(BOOKS)}).")
 
 
@@ -63,6 +68,8 @@ async def add_book(book: Book):
     with open(BOOKS_FILE, "w") as f:
         json.dump(BOOKS, f)
 
+    logger.info("Book added successfully to JSON file")
+
     return {"book_id": book.book_id}
 
 
@@ -70,6 +77,8 @@ async def add_book(book: Book):
 async def get_book(book_id: str):
     for book in BOOKS:
         if book.book_id == book_id:
+            logger.info(f"Book with {book_id} found successfully")
             return book
 
+    logger.error(f"Book with {book_id} not found")
     raise HTTPException(404, f"Book ID {book_id} not found in database.")
